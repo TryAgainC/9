6ea4d19d7bb6cc000e318658f5079dfe9a30ee90
TryAgainC <63404307+TryAgainC@users.noreply.github.com>
2025-03-21 01:28:59 +0800

    Update tgsign.py

--- tgsign.py
+++ tgsign.py
@@ -12,7 +12,7 @@ for num in range(len(api_id)):
 	client = TelegramClient(session_name[num], api_id[num], api_hash[num])
 	client.start()
 	client.send_message("@nb3344bot", '签到')	#第一项是机器人ID，第二项是发送的文字
-        client.send_message("@jdHappybot", '签到')	#第一项是机器人ID，第二项是发送的文字
+	client.send_message("@jdHappybot", '签到')	#第一项是机器人ID，第二项是发送的文字
 	time.sleep(5)	#延时5秒，等待机器人回应（一般是秒回应，但也有发生阻塞的可能）
 	client.send_read_acknowledge("@luxiaoxun_bot")	#将机器人回应设为已读
 	print("Done! Session name:", session_name[num])
